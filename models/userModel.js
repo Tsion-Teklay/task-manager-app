36784320949a44f2f5014aca494c4e51eed5a954
Tsion Teklay <tsionteklay18@gmail.com>
2024-09-02 20:48:23 +0300

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        match: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/ // Email validation regex
+    },
     password: { type: String, required: true },
     role: { type: String, enum: ['user', 'admin'], required: true }
 });
